1221e84784b4689cc380bc603e5696cda20aef62
aiman-zaheer <aimanzaheer2001@gmail.com>
2023-09-25 03:10:36 -0700

    fix: code format and encrpytion password

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const express = require("express");
-const connection = require("./config/database.config.js");
 const routes = require("./routes/index.js");
 const externalSettings = require("./config/middleWares.config.js");
 
-//accessig environment variable
-const API_PORT = process.env.API_PORT;
-const PORT = API_PORT || 4000;
 const app = express();
 
-//connecting database
-connection();
-
+//middleware
 externalSettings(app);
 
 //routes
 routes(app);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+module.exports = app;
--- routes/authentication/authRoutes.js
+++ routes/authentication/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 // const {
 //   logInController,
 //   signUpController,
@@ -9,8 +10,17 @@ const router = express.Router();
 
 router.post("/signUp", async (req, res) => {
   try {
-    const ss = await UserModel.create(req.body);
-    res.status(200).json({ data: ss, success: "true", status: 200 });
+    const { email } = req.body.email;
+    const ss = await UserModel.findOne({ email });
+    if (ss) {
+      res.status(200).json({ data: null, success: "false", status: 403 });
+    } else {
+      const { password, ...restbody } = req.body;
+      const encryptedPassword = await bcrypt.hash(password, 10);
+      const data = { ...restbody, password: encryptedPassword };
+      const user = await UserModel.create(data);
+      res.status(200).json({ data: user, success: "true", status: 200 });
+    }
   } catch (error) {
     res.status(500).json({ data: null, success: "false", status: 500 });
     console.log(`${error}`);
@@ -30,4 +40,5 @@ router.post("/login", async (req, res) => {
     console.log(`${error}`);
   }
 });
+
 module.exports = router;
