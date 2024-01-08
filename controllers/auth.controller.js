368c4bc2ab53ec6de4f8c43958a8d308e531e517
aiman-zaheer <aimanzaheer2001@gmail.com>
2024-01-07 22:26:52 -0800

    change5

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -24,8 +24,6 @@ const signUpController = async (req, res) => {
       const user = await UserModel.create(data);
       const dataObject = user.toObject();
       const { email, password: pass, ...restData } = dataObject;
-
-      
       sendResponse(res, restData, true, 200, "ok");
     }
   } catch (error) {
